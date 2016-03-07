e870938b54597eabae700197ad681efd811bffc3
bmihaylov33 <bob4oo_levski@mail.bg>
2016-03-07 11:45:48 +0200

    adding

---
+++ PostfixCalaculator/src/org/elsys/postfix/Minus.java
@@ -0,0 +1,14 @@
+package org.elsys.postfix;
+
+public class Minus extends BinaryOperation {
+	 
+	public Minus() {
+		// TODO Auto-generated constructor stub
+		super("-");
+	}
+	 
+	@Override
+	public double calc(double v1, double v2) {
+		return v2 - v1;
+	}
+}
